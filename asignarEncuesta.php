bc24333ae0079f1244504642f5e0e088f0b973f8
julianTesla <juliandesarrollotesla@gmail.com>
2022-11-08 16:18:32 -0300

    asignar encuesta terminado

--- asignarEncuesta.php
+++ asignarEncuesta.php
@@ -7,11 +7,11 @@ include "conexion/conex.php";
     <div class="row g-4">
         <div class="col-sm-12 col-xl-6">
             <div class="bg-secondary rounded h-100 p-4">
-                <form action="/back/asignar_encuesta.php" method="POST">
+                <form id= "asignar_encuesta">
                     <h6 class="mb-3">Asignar encuesta</h6>
 
                     <!-- SELECT ENCUESTAS -->
-                    <select name="encuesta" class="form-select mb-3" id="selectEncuesta" onchange="selectEncuesta();">
+                    <select name="encuesta_id" class="form-select mb-3" id="encuesta_id" >
                         <option value="0">Seleccionar encuesta</option>
                         <?php
                         //TRAEMOS LAS ENCUESTAS DISPONIBLES DE LA BASE DE DATOS
@@ -25,7 +25,7 @@ include "conexion/conex.php";
                     <!-- FIN SELECT ENCUESTAS -->
 
                     <!-- SELECT CURSO -->
-                    <select name="curso" class="form-select mb-3" id="selectCurso" onchange="selectCurso();">
+                    <select name="curso_id" class="form-select mb-3" id="curso_id" >
                         <option value="0">Seleccionar curso</option>
                         <?php
                         //TRAEMOS LAS CURSOS DISPONIBLES DE LA BASE DE DATOS
@@ -37,10 +37,9 @@ include "conexion/conex.php";
                         ?>
                     </select>
                     <!-- FIN SELECT CURSO -->
-
-                    <input type="submit" class="btn btn-primary mt-3" value="Enviar encuesta">
+                    <button class="btn btn-primary mt-3" onclick="enviar_encuesta();">Enviar encuesta</button>
                 </form><br>
-
+                   <div id="respuesta"></div>
                 <!-- <div class="alert alert-success alert-dismissible fade show" role="alert">
                     ¡Encuesta enviada correctamente!
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -52,7 +51,22 @@ include "conexion/conex.php";
 </div>
 
 
+<script type="text/javascript">
 
+    function enviar_encuesta() {
+        event.preventDefault();
+        var curso_id=  document.getElementById('curso_id').value;
+        var encuesta_id= document.getElementById('encuesta_id').value;
+        $.ajax({
+            type: 'POST',
+            url: 'back/asignar_encuesta.php',
+            data: {"curso_id": curso_id ,"encuesta_id":encuesta_id},
+            success: function(r) {
+                $('#respuesta').html(r);
+            }
+        });
+    }
+</script>
 
 
 <?php
--- back/asignar_encuesta.php
+++ back/asignar_encuesta.php
@@ -1,15 +1,24 @@
-// function selectencuesta(){
-//     let selectencuesta = document.getElementById('selectencuesta');
-//     let encuestaId = selectencuesta.value;
-    
-//     document.getElementById("encuestaSeleccionada").innerText = `Encuesta ${encuestaId}`;
-// };
+<?php
+include "../conexion/conex.php";
 
+$encuesta_id = $_POST['encuesta_id'];
+$curso_id = $_POST['curso_id'];
 
-// let select = document.getElementById("buscar_encuesta"); /*Obtener el SELECT */
-// let encId = select.options[select.selectedIndex].value; /* Obtener el valor */
+if ($encuesta_id == 0 || $curso_id == 0) {
+    echo '<div class="alert alert-primary alert-dismissible fade show" role="alert"><i class="fa fa-exclamation-circle me-2"></i>
+¡Debe seleccionar las opciones!
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+    die();
+}
 
+$sql = "INSERT INTO asignacion_encuesta (curso_id, encuesta_id) VALUE ('$curso_id', '$encuesta_id')";
+$resultado = mysqli_Query($conex, $sql);
 
+echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+¡Encuesta enviada correctamente!
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
 
-// let select = document.getElementById("buscar_curso"); /*Obtener el SELECT */
-// let curId = select.options[select.selectedIndex].value; /* Obtener el valor */
+mysqli_close($conex);
+?>
