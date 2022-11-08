4a4afd95eddce4ea0a180bace40f54c029ce254b
julianTesla <juliandesarrollotesla@gmail.com>
2022-11-08 14:59:52 -0300

    modifique lista encuesta

--- listaEncuestas.php
+++ listaEncuestas.php
@@ -45,12 +45,12 @@ include "conexion/conex.php";
 <div id="respuesta"></div>
 
 <?php
-$sql2= "SELECT DISTINCT nombre_encuesta, nombre_pregunta, tipo 
+$sql2= "SELECT nombre_encuesta, nombre_pregunta, tipo 
 FROM encuestas,preguntas, tipos_preguntas, opciones WHERE encuestas.id_encuesta = preguntas.encuesta_id 
 AND preguntas.id_pregunta = opciones.pregunta_id AND tipos_preguntas.id_tipo_pregunta = preguntas.tipo_pregunta_id";
 $resultado2= mysqli_Query($conex,$sql2);
 while($row2= mysqli_fetch_array($resultado2))
-{ array_unique($row2);
+{ 
 ?>
 <!-- INICIO ENCUESTA -->
 <div class="container-fluid pt-4 px-4">
@@ -100,26 +100,7 @@ while($row2= mysqli_fetch_array($resultado2))
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                 </svg></td>
                     </tr>
-                    <tr>
-                        <td>Pregunta 2</td>
-                        <td>Multiple choice</td>
-                        <td>1- Opcion a<br>
-                            2- Opcion b<br>
-                            3- Opcion c</td>
-                        <td><button type="submit" class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                </svg></td>
-                    </tr>
-                    <tr>
-                        <td>Pregunta 3</td>
-                        <td>Descripción</td>
-                        <td>Campo de texto</td>
-                        <td><button type="submit" class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                </svg></td>
-                    </tr>
+                    
                 </tbody>
             </table>
         </div>
