d89c081111137b2168ba9f9939af06ade2093e36
julianTesla <juliandesarrollotesla@gmail.com>
2022-11-10 19:52:53 -0300

    trabaje en la parte de modificar las preguntas

---
+++ back/guardar_modificacion_pregunta.php
@@ -0,0 +1,65 @@
+<?php
+include "../conexion/conex.php";
+
+$id_pregunta= $_POST['id_pregunta'];
+$nombre_pregunta=['nombre_pregunta'];
+$id_opcion1= $_POST['id_opcion1'];
+$id_opcion2= $_POST['id_opcion2'];
+$id_opcion3= $_POST['id_opcion3'];
+$id_opcion4= $_POST['id_opcion4'];
+$id_opcion5= $_POST['id_opcion5'];
+$id_texto= $_POST['id_texto'];
+
+$opcion1= $_POST['opcion1'];
+$opcion1= $_POST['opcion2'];
+$opcion1= $_POST['opcion3'];
+$opcion1= $_POST['opcion4'];
+$opcion1= $_POST['opcion5'];
+$texto= $_POST['texto'];
+
+if( $nombre_pregunta != false)
+{
+$sql= "UPDATE `preguntas` SET `nombre_pregunta` = '$nombre_pregunta' WHERE `preguntas`.`id_pregunta` = '$id_pregunta'";
+$resultado= mysqli_query($conex, $sql);
+}
+
+if( $opcion1 != false)
+{
+    $sql_opcion1= "UPDATE `opciones` SET `descripcion` = '$opcion1' WHERE `opciones`.`id_opciones` = '$id_opcion1'";
+    $resultado_opcion1= mysqli_Query($conex, $sql_opcion1);
+}
+
+if($opcion2 != false)
+{
+    $sql_opcion2= "UPDATE `opciones` SET `descripcion` = '$opcion2' WHERE `opciones`.`id_opciones` = '$id_opcion2'";
+    $resultado_opcion2= mysqli_Query($conex, $sql_opcion2);
+}
+
+if( $opcion3 != false)
+{
+    $sql_opcion3= "UPDATE `opciones` SET `descripcion` = '$opcion3' WHERE `opciones`.`id_opciones` = '$id_opcion3'";
+    $resultado_opcion3= mysqli_Query($conex, $sql_opcion3);
+}
+
+if( $opcion4 != false)
+{
+    $sql_opcion4= "UPDATE `opciones` SET `descripcion` = '$opcion4' WHERE `opciones`.`id_opciones` = '$id_opcion4'";
+    $resultado_opcion4= mysqli_Query($conex, $sql_opcion4);
+}
+
+if( $opcion5 != false)
+{
+    $sql_opcion5= "UPDATE `opciones` SET `descripcion` = '$opcion5' WHERE `opciones`.`id_opciones` = '$id_opcion5'";
+    $resultado_opcion5= mysqli_Query($conex, $sql_opcion5);
+}
+
+if($texto != false)
+{
+    $sql_texto= "UPDATE `opciones` SET `descripcion` = '$texto' WHERE `opciones`.`id_opciones` = '$id_texto'";
+    $resultado_texto= mysqli_Query($conex, $sql_texto);
+}
+mysqli_close($conex);
+?>
+<!-- <script>
+window.location="../listaEncuestas.php";
+</script> -->
--- modificar_preguntas.php
+++ modificar_preguntas.php
@@ -2,72 +2,90 @@
 include "parte_superior.php";
 include "conexion/conex.php";
 
-//resivimos las variables de la encuesta y pregunta a modificar
-$id_encuesta =$_GET['id_encuesta'];
-$id_pregunta =$_GET['id_pregunta'];
+$id_encuesta=$_GET['id_encuesta'];
+$id_pregunta=$_GET['id_pregunta'];
 
 $sql_encuesta = "SELECT nombre_encuesta, id_encuesta FROM encuestas 
-WHERE encuestas.id_encuestas = '$id_encuesta' ORDER BY id_encuesta DESC LIMIT 1;";
+WHERE encuestas.id_encuesta = '$id_encuesta' ORDER BY id_encuesta DESC LIMIT 1;";
 $resultado = mysqli_Query($conex, $sql_encuesta);
-
-while ($row = mysqli_fetch_array($resultado)) {
+while ($row = mysqli_fetch_array($resultado)) 
+{
     $grupo_id = $row[1];
     $nombre_encuesta = $row[0];
+
+    $sql1="SELECT  id_pregunta, nombre_pregunta, tipo_pregunta_id FROM preguntas WHERE preguntas.id_pregunta = '$id_pregunta'";
+    $resultado1= mysqli_query($conex,$sql1);
+    while($row1= mysqli_fetch_array($resultado1))
+    {
+        $id_pregunta=$row1['id_pregunta'];
+        $nombre_pregunta=$row1['nombre_pregunta'];
+        $tipo_pregunta_id= $row1['tipo_pregunta_id'];
+    }
 ?>
 
     <!-- Form Start -->
     <div class="container-fluid pt-4 px-4">
         <div class="row g-4">
+            
             <div class="col-sm-12 col-xl-6">
                 <div class="bg-secondary rounded h-100 p-4">
-                    <form action="back/guardar_pregunta.php" method="POST">
-                        <h4> <?php echo $row[0];   ?> </h4>
+                    <form action="back/guardar_modificacion_pregunta.php" method="POST">
+                        <h4> <?php echo $row[0]; }  ?> </h4>
                         <h6 class="mb-4">Ingrese la pregunta</h6>
-                        <input style="position: absolute; visibility: hidden;" type="int" name="encuesta_id" value=" <?php echo $row[1]; } ?>">
+                        <input style="position: absolute; visibility: hidden;" type="int" name="id_pregunta" value=" <?php echo $id_pregunta; ?>">
                         <div class="row mb-3">
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Pregunta:</label>
                             <div class="col-sm-10">
-                                <input name="nombre_pregunta" type="Text" class="form-control" id="inputEmail3" required>
+                                <input name="nombre_pregunta" type="text" class="form-control" id="inputEmail3" required value="<?php echo $nombre_pregunta;?>">
                             </div>
                         </div>
 
                         <fieldset class="row mb-3">
-                            <legend class="col-form-label col-sm-2 pt-0">Tipo de pregunta</legend>
+                           
                             <div class="col-sm-10">
-                                <input class="form-check-input" type="radio" name="tipo_pregunta" id="gridRadios1" value="1" checked onclick="mostrar_checkbox();">
-                                <label class="form-check-label" for="gridRadios1">
-                                    Múltiple choice
-                                </label>
-                                <input class="form-check-input" type="radio" name="tipo_pregunta" id="gridRadios1" value="2" onclick="mostrar_texto();">
-                                <label class="form-check-label">
-                                    Texto
-                                </label>
-                                <div class="form-chek">
+                                <?php
+                                if($tipo_pregunta_id == 1)
+                                {
+                                    $sql2="SELECT pregunta_id, descripcion, id_opciones 
+                                    FROM preguntas, tipos_preguntas,encuestas,opciones 
+                                    WHERE preguntas.tipo_pregunta_id = tipos_preguntas.id_tipo_pregunta 
+                                    AND encuestas.id_encuesta = preguntas.encuesta_id 
+                                    AND preguntas.id_pregunta = opciones.pregunta_id ORDER BY id_opciones";
+                                    $resultado2= mysqli_query($conex,$sql2);
+                                   
+                                    echo '<input class="form-check-input" type="radio" name="tipo_pregunta" id="gridRadios1" value="1" checked onclick="mostrar_checkbox();" style="position: absolute; visibility: hidden;">
+                                    <div class="form-chek">';
+                                    $cont= 1;
+                                    while($row2= mysqli_fetch_array($resultado2))
+                                    {
+                                        if($id_pregunta == $row2[0])
+                                        {
+                                        echo '<label>Opcion'.$cont.':</label> <input class="form-control" type="text" name="opcion_'.$cont.'" id="descripcion'.$cont.'" value="'.$row2[1].'">';
+                                        echo '<input style="position: absolute; visibility: hidden;" type="int" name="id_opcion'.$cont.'" value="'.$row2[2].'">';
+                                        $cont++;
+                                        }
+                                    }
+                                   echo '</div>';
+                                }
+                                elseif($tipo_pregunta_id == 2)
+                                {
+                                    $sql2="SELECT pregunta_id, descripcion FROM preguntas, tipos_preguntas,encuestas,opciones 
+                                    WHERE preguntas.tipo_pregunta_id = tipos_preguntas.id_tipo_pregunta 
+                                    AND encuestas.id_encuesta = preguntas.encuesta_id AND preguntas.id_pregunta = opciones.pregunta_id ORDER BY id_opciones";
+                                    $resultado2= mysqli_query($conex,$sql2);
 
-                                    <div id="checkbox">
-                                        <label>Opcion 1:</label> <input class="form-control" type="text" name="opcion_1" id="descripcion1">
-                                        <label>Opcion 2:</label> <input class="form-control" type="text" name="opcion_2" id="descripcion2">
-                                        <button type="button" class="btn btn-sm btn-secondary m-2" id="boton3" onclick="agregar_opcion3()">+ Agregar opción</button>
-                                        <div id="opcion3">
-                                            <label>Opcion 3:</label> <input class="form-control" type="text" name="opcion_3" id="descripcion3">
-                                            <button type="button" class="btn btn-sm btn-secondary m-2" id="boton4" onclick="agregar_opcion4()">+ Agregar opción</button>
-                                        </div>
-
-                                        <div id="opcion4">
-                                            <label>Opcion 4:</label> <input class="form-control" type="text" name="opcion_4" id="descripcion4">
-                                            <button type="button" class="btn btn-sm btn-secondary m-2" id="boton5" onclick="agregar_opcion5()">+ Agregar opción</button>
-                                        </div>
-
-                                        <div id="opcion5">
-                                            <label>Opcion 5:</label> <input class="form-control" type="text" name="opcion_5" id="descripcion5">
-                                        </div>
-
-                                    </div>
-                                    <div id="texto">
-                                        <textarea class="form-control" name="descripcion_texto" id="floatingTextarea" cols="30" rows="13" placeholder="Ingrese la marca de agua a mostrar"></textarea>
-                                    </div>
-
-                                </div>
+                                    echo '<input class="form-check-input" type="radio" name="tipo_pregunta" id="gridRadios1" value="2" onclick="mostrar_texto();" style="position: absolute; visibility: hidden;">
+                                    <div class="form-chek">';
+                                    while($row2= mysqli_fetch_array($resultado2))
+                                    {
+                                        if($id_pregunta == $row2[0])
+                                        {
+                                      echo  '<textarea class="form-control" name="descripcion_texto" id="floatingTextarea" cols="30" rows="13" placeholder="Ingrese la marca de agua a mostrar">'.$row2[1].'</textarea>';
+                                        }
+                                    }
+                                        echo '</div>';
+                                }
+                                ?>
                             </div>
                         </fieldset>
 
@@ -77,72 +95,6 @@ while ($row = mysqli_fetch_array($resultado)) {
                     <a href="resultados.php" class="btn btn-outline-primary w-100 m-2">Finalizar</a>
                 </div>
             </div>
-
-            <?php
-            // TRAEMOS LOS DATOS DE LAS PREGUNTAS CREADAS 
-            $sql = "SELECT id_encuesta, nombre_encuesta FROM encuestas WHERE encuestas.id_encuesta = '$grupo_id' ";
-            $resultado = mysqli_Query($conex, $sql);
-
-            if (mysqli_num_rows($resultado) > 0) {
-                while ($row = mysqli_fetch_array($resultado)) {
-                    $sql2 = "SELECT encuesta_id, id_pregunta, nombre_pregunta, tipo 
-    FROM encuestas,preguntas,tipos_preguntas WHERE encuestas.id_encuesta = '$grupo_id' 
-    AND encuestas.id_encuesta = preguntas.encuesta_id 
-    AND preguntas.tipo_pregunta_id = tipos_preguntas.id_tipo_pregunta";
-                    $resultado2 = mysqli_Query($conex, $sql2);
-            ?>
-                    <!--Inicio de tabla-->
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h5 class="mb-4"> <?php echo $nombre_encuesta; ?> </h5>
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Preguntas</th>
-                                        <th scope="col">Tipo de pregunta</th>
-                                        <th scope="col">Descripcion</th>
-                                    </tr>
-                                </thead>
-                                <?php
-                                while ($row2 = mysqli_fetch_array($resultado2)) {
-                                    if ($row[0] == $row2[0]) {
-                                        $contador = 1;
-                                ?>
-                                        <tbody>
-                                            <tr>
-                                        <?php
-                                        echo  '<td>' . $row2[1] . '</td>';
-                                        echo '<td>' . $row2[2] . '</td>';
-                                        echo '<td>' . $row2[3] . '</td>';
-                                        echo '<td>';
-                                        $sql3 = "SELECT pregunta_id, descripcion 
-                                            FROM preguntas, tipos_preguntas,encuestas,opciones 
-                                            WHERE preguntas.tipo_pregunta_id = tipos_preguntas.id_tipo_pregunta 
-                                            AND encuestas.id_encuesta = preguntas.encuesta_id 
-                                            AND preguntas.id_pregunta = opciones.pregunta_id ORDER BY id_opciones";
-                                        $resultado3 = mysqli_query($conex, $sql3);
-                                        while ($row3 = mysqli_fetch_array($resultado3)) {
-
-                                            if ($row2[1] == $row3[0]) {
-                                                echo $contador . '-' . $row3[1] . '<br>';
-                                                $contador++;
-                                            }
-                                        }
-                                        echo '</td>';
-                                    }
-                                }
-                                        ?>
-                                            </tr>
-                                        </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <!--Fin de tabla-->
-            <?php
-                }
-            }
-            ?>
         </div>
     </div>
     <!-- Form End -->
