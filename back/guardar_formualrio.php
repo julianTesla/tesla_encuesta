2f42f59313961ba65bb0218eb065b8d68157df83
desarrollotesla <desarrollotesla1@gmail.com>
2022-11-17 14:33:18 -0300

    Agrego pantalla de agradecimiento respuesta por responder encuesta

--- back/guardar_formualrio.php
+++ back/guardar_formualrio.php
@@ -1,106 +1,140 @@
 <?php
 include "../conexion/conex.php";
 
-ini_set('date.timezone','America/Argentina/Buenos_Aires');
+ini_set('date.timezone', 'America/Argentina/Buenos_Aires');
 
-if(isset($_POST['respuesta1']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $respuesta_1= $_POST['respuesta1'];
+if (isset($_POST['respuesta1'])) {
+    $fecha = date('Y-m-d', time());
+    $respuesta_1 = $_POST['respuesta1'];
 
-    $sql1="INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql1 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,0, '$respuesta_1','$fecha' )";
-    $resultado1= mysqli_query($conex, $sql1);
+    $resultado1 = mysqli_query($conex, $sql1);
 }
 
-if(isset($_POST['respuesta2']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $respuesta_2= $_POST['respuesta2'];
+if (isset($_POST['respuesta2'])) {
+    $fecha = date('Y-m-d', time());
+    $respuesta_2 = $_POST['respuesta2'];
 
-    $sql2="INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql2 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,0, '$respuesta_2','$fecha' )";
-    $resultado2= mysqli_query($conex, $sql2);
+    $resultado2 = mysqli_query($conex, $sql2);
 }
 
-if(isset($_POST['respuesta3']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $respuesta_3= $_POST['respuesta3'];
+if (isset($_POST['respuesta3'])) {
+    $fecha = date('Y-m-d', time());
+    $respuesta_3 = $_POST['respuesta3'];
 
-    $sql3="INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql3 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,0, '$respuesta_3','$fecha' )";
-    $resultado3= mysqli_query($conex, $sql3);
+    $resultado3 = mysqli_query($conex, $sql3);
 }
 
-if(isset($_POST['respuesta4']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $respuesta_4= $_POST['respuesta4'];
+if (isset($_POST['respuesta4'])) {
+    $fecha = date('Y-m-d', time());
+    $respuesta_4 = $_POST['respuesta4'];
 
-    $sql4="INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql4 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,0, '$respuesta_4','$fecha' )";
-    $resultado4= mysqli_query($conex, $sql4);
+    $resultado4 = mysqli_query($conex, $sql4);
 }
 
-if(isset($_POST['respuesta5']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $respuesta_5= $_POST['respuesta5'];
+if (isset($_POST['respuesta5'])) {
+    $fecha = date('Y-m-d', time());
+    $respuesta_5 = $_POST['respuesta5'];
 
-    $sql5="INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql5 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,0, '$respuesta_5','$fecha' )";
-    $resultado5= mysqli_query($conex, $sql5);
+    $resultado5 = mysqli_query($conex, $sql5);
 }
 
-if(isset($_POST['texto1']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $texto1= $_POST['texto1'];
+if (isset($_POST['texto1'])) {
+    $fecha = date('Y-m-d', time());
+    $texto1 = $_POST['texto1'];
 
-    $sql_texto1= "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql_texto1 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,'$texto1', 0,'$fecha' )";
-    $resultado_texto1= mysqli_query($conex, $sql_texto1);
+    $resultado_texto1 = mysqli_query($conex, $sql_texto1);
 }
 
-if(isset($_POST['texto2']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $texto2= $_POST['texto2'];
+if (isset($_POST['texto2'])) {
+    $fecha = date('Y-m-d', time());
+    $texto2 = $_POST['texto2'];
 
-    $sql_texto2= "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql_texto2 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,'$texto2', 0,'$fecha' )";
-    $resultado_texto2= mysqli_query($conex, $sql_texto2);
+    $resultado_texto2 = mysqli_query($conex, $sql_texto2);
 }
 
-if(isset($_POST['texto3']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $texto3= $_POST['texto3'];
+if (isset($_POST['texto3'])) {
+    $fecha = date('Y-m-d', time());
+    $texto3 = $_POST['texto3'];
 
-    $sql_texto3= "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql_texto3 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,'$texto3', 0,'$fecha' )";
-    $resultado_texto3= mysqli_query($conex, $sql_texto3);
+    $resultado_texto3 = mysqli_query($conex, $sql_texto3);
 }
 
-if(isset($_POST['texto4']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $texto4= $_POST['texto4'];
+if (isset($_POST['texto4'])) {
+    $fecha = date('Y-m-d', time());
+    $texto4 = $_POST['texto4'];
 
-    $sql_texto4= "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql_texto4 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,'$texto4', 0,'$fecha')";
-    $resultado_texto4= mysqli_query($conex, $sql_texto4);
+    $resultado_texto4 = mysqli_query($conex, $sql_texto4);
 }
 
-if(isset($_POST['texto5']))
-{
-    $fecha=date('Y-m-d',time()); 
-    $texto5= $_POST['texto5'];
+if (isset($_POST['texto5'])) {
+    $fecha = date('Y-m-d', time());
+    $texto5 = $_POST['texto5'];
 
-    $sql_texto5= "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
+    $sql_texto5 = "INSERT INTO resultados (asignacion_encuesta_id, respuesta_text, respuesta_multiplechoice, fecha) 
     VALUE(4,'$texto5', 0,'$fecha')";
-    $resultado_texto5= mysqli_query($conex, $sql_texto5);
+    $resultado_texto5 = mysqli_query($conex, $sql_texto5);
 }
 mysqli_close($conex);
 ?>
+
+
+
+<!-- HTML AGRADECIMIENTO RESPUESTA  -->
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <title>Encuesta</title>
+</head>
+
+<body>
+    <div class="mx-0 mx-sm-auto">
+        <div class="card">
+            <div class="card-header p-0" style="background-color:#303030">
+                <h5 class="card-title text-white mt-2 text-center" id="exampleModalLabel">
+                    <img src="../popup/TESLA_LogoBlanco.png" class="logo m-2" style="width: 170px">
+                </h5>
+            </div>
+            <div class="modal-body">
+                <div class="text-center">
+                    <i class="far fa-file-alt fa-4x text-primary"></i>
+                    <h3 class="m-4">
+                        <strong>¡Gracias por ayudarnos a seguir creciendo!</strong>
+                    </h3>
+                    <img src="../popup/comprobado.png" width="100px" alt="Check">
+                </div>
+                <br>
+                <br>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
