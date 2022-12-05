a558e9403dccd9a7ca5dff33668227c185077e85
julianTesla <117113698+julianTesla@users.noreply.github.com>
2022-12-05 11:34:14 -0300

    ya esta todo listo

--- back/filtrar_encuestas.php
+++ back/filtrar_encuestas.php
@@ -1,33 +1,36 @@
 <?php
-/* SELECT DISTINCT nombre_encuesta, nombre_curso 
-FROM resultados, encuestas, cursos 
-WHERE resultados.resultado_encuesta_id = encuestas.id_encuesta 
-AND resultados.resultado_curso_id = cursos.id_curso 
-AND resultados.respuesta_multiplechoice != '0' AND cursos.id_curso = 255 AND encuestas.id_encuesta = 12
-*/
 include "../conexion/conex.php";
 $curso = $_POST['curso'];
 $encuesta = $_POST['encuesta'];
 $f1 = $_POST['f1'];
 $f2 = $_POST['f2'];
 
-$sqlENC= "SELECT * FROM encuestas ";
+$sqlENC= "SELECT DISTINCT id_encuesta, nombre_encuesta, nombre_curso, id_curso 
+FROM resultados, encuestas, cursos 
+WHERE resultados.resultado_encuesta_id = encuestas.id_encuesta 
+AND resultados.resultado_curso_id = cursos.id_curso 
+AND resultados.respuesta_multiplechoice != '0'";
 if($encuesta > 0)
 {
-    $sqlENC .= " WHERE encuestas.id_encuesta = '$encuesta'";
+    $sqlENC .= "  AND encuestas.id_encuesta = '$encuesta' ";
+}
+if($curso > 0)
+{
+    $sqlENC .= " AND cursos.id_curso = '$curso' ";
 }
 $resultadoENC= mysqli_Query($conex,$sqlENC);
 
 while($rowENC= mysqli_fetch_array($resultadoENC))
 {
     $id_encuesta= $rowENC['id_encuesta'];
+    $id_curso= $rowENC['id_curso'];
 
        echo '<!-- INICIO CONTENEDOR ENCUESTA -->
        <div class="col-sm-12 col-md-6 col-xl-4">
             <div class="h-100 bg-secondary rounded p-4" id=encuesta>
 
                 <div class="bg-secondary text-center rounded p-4">
-                    <h2>'.$rowENC['nombre_encuesta'].'</h2>
+                    <h2>'.$rowENC['nombre_encuesta'].'</h2>'.$rowENC['nombre_curso'].'
                 </div>';
                 
 
@@ -62,12 +65,16 @@ if ($encuesta > 0) {
 if ($curso > 0) {
     $sqlRES .= " AND resultados.resultado_curso_id  = '$curso' ";
 }
+else
+{
+    $sqlRES .= " AND resultados.resultado_curso_id  = '$id_curso' ";
+}
 if ($f1 != false || $f2 != false) {
     $sqlRES .= " AND resultados.fecha BETWEEN '$f1' AND '$f2' ";
 }
 $resultadoRES= mysqli_query($conex, $sqlRES);
 
-if(mysqli_num_rows($resultadoRES)>0)
+if (mysqli_num_rows($resultadoRES)>0)
 {
     
     $acum= 0;
@@ -86,7 +93,6 @@ if(mysqli_num_rows($resultadoRES)>0)
 }
 else
 { $total= 0; }
-//$total = round( 100/$acum*$contOPC);
                         echo'<h6>'.$rowOPC['descripcion'].'</h6>
                         <div class="pg-bar mb-3">
                             <div class="progress">
